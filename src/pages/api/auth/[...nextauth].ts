c8935083e30551216f89f29a5884168cb61e304f
Mantton <mantton.official@gmail.com>
2022-09-03 16:19:15 -0400

    Initial Init

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    TwitterProvider({
+      clientId: env.TWITTER_CLIENT_ID,
+      clientSecret: env.TWITTER_CLIENT_SECRET,
+      version: "2.0",
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
