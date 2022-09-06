db35d4a0c012052ca1ab07f4831effa2b76d5b4b
Mantton <mantton.official@gmail.com>
2022-09-06 10:35:20 -0400

    ci: implement commitlint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        purple_primary: "#95A2F1",
-        primary: "#000000",
-        legendary: "#f8961e",
-        ultra_rare: "#7b2cbf",
-        rare: "#00c0f0",
-        uncommon: "#ae2012",
-        common: "#0ead69",
-        dynamic_color: "var(--color)",
-      },
-      backgroundImage: {
-        dynamic_image: "var(--url)",
-      },
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                purple_primary: '#95A2F1',
+                primary: '#000000',
+                legendary: '#f8961e',
+                ultra_rare: '#7b2cbf',
+                rare: '#00c0f0',
+                uncommon: '#ae2012',
+                common: '#0ead69',
+                dynamic_color: 'var(--color)',
+            },
+            backgroundImage: {
+                dynamic_image: 'var(--image)',
+            },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
